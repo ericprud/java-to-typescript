aa12be03c3bd9e21aeb5b0eab15b2893203de3ad
Eric Prud'hommeaux <eric+github@w3.org>
2021-11-19 14:01:12 +0100

    + System.out.println -> console.log

--- javatots/src/main/java/org/javatots/transformers/JavaCoreTypesVisitor.java
+++ javatots/src/main/java/org/javatots/transformers/JavaCoreTypesVisitor.java
@@ -1,5 +1,8 @@
 package org.javatots.transformers;
 
+import com.github.javaparser.ast.expr.Expression;
+import com.github.javaparser.ast.expr.MethodCallExpr;
+import com.github.javaparser.ast.expr.NameExpr;
 import com.github.javaparser.ast.type.ClassOrInterfaceType;
 import com.github.javaparser.ast.type.PrimitiveType;
 import com.github.javaparser.ast.visitor.ModifierVisitor;
@@ -9,15 +12,27 @@ import com.github.javaparser.ast.visitor.Visitable;
  * Change java-native scalar types to corresponding typescript (scalar) types.
  */
 public class JavaCoreTypesVisitor extends ModifierVisitor<Void> {
+    /**
+     * Map Java built-in boxed types to Typescript
+     * @param n AST class/interface type node
+     * @param arg ignored
+     * @return replacement node
+     */
     @Override
     public Visitable visit(final ClassOrInterfaceType n, final Void arg) {
         switch (n.getName().asString()) {
             case "String": n.setName("string"); break;
             case "Integer": n.setName("number"); break;
-            case "int": n.setName("number"); break;
         }
         return super.visit(n, arg);
     }
+
+    /**
+     * Turn primitive Java ints to Typescript numbers
+     * @param n AST primitie type node
+     * @param arg ignored
+     * @return replacement node
+     */
     @Override
     public Visitable visit(final PrimitiveType n, final Void arg) {
         switch (n.getType()) {
@@ -26,5 +41,53 @@ public class JavaCoreTypesVisitor extends ModifierVisitor<Void> {
             default:
                 return super.visit(n, arg);
         }
+    }
+
+    /**
+     * Map built-in Java calls to Typescript
+     * e.g. `System.out.println(c);`
+     *   -> `console.log(c);`
+     * @param n Java method call node
+     * @param arg ignored
+     * @return replacement node
+     */
+    public Visitable visit(final MethodCallExpr n, final Void arg) {
+        n.getScope().ifPresent(functionScope -> {
+            functionScope.ifFieldAccessExpr(output -> {
+                Expression system = output.getScope();
+                output.getScope().ifNameExpr(matchSystem -> {
+                    if (matchSystem.getNameAsString().equals("System")) {
+                        String consoleFunction = null;
+                        String unixName = null;
+                        switch (output.getName().asString()) {
+                            case "out":
+                                consoleFunction = "log";
+                                unixName = "stdout";
+                                break;
+                            case "error":
+                                consoleFunction = "err";
+                                unixName = "stderr";
+                                break;
+                        }
+                        if (unixName != null) {
+                            switch (n.getNameAsString()) {
+                                case "println":
+                                    // set a new scope
+                                    n.setScope(new NameExpr("console"));
+                                    n.setName(consoleFunction);
+                                    break;
+                                case "print":
+                                    // leverage the fact that we need the same scope depth
+                                    matchSystem.setName("process");
+                                    output.setName(unixName);
+                                    n.setName("write");
+                                    break;
+                            }
+                        }
+                    }
+                });
+            });
+        });
+        return super.visit(n, arg);
     }
 }
